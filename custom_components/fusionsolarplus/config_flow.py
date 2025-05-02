4c88b87cecee5fbb8926270873df2d3ad9e13d6f
Jort <63785276+JortvanSchijndel@users.noreply.github.com>
2025-05-02 16:53:16 +0200

    Fixed imports

--- custom_components/fusionsolarplus/config_flow.py
+++ custom_components/fusionsolarplus/config_flow.py
@@ -1,3 +1,33 @@
+import logging
+import asyncio
+import subprocess
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from custom_components.fusionsolarplus.const import (
+    DOMAIN,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_DEVICE_TYPE,
+    CONF_DEVICE_ID,
+    CONF_DEVICE_NAME
+)
+from .api.fusion_solar_py.client import FusionSolarClient
+from .api.fusion_solar_py.exceptions import AuthenticationException
+
+_LOGGER = logging.getLogger(__name__)
+
+DEVICE_TYPE_PLANT = "Plant"
+DEVICE_TYPE_INVERTER = "Inverter"
+
+DEVICE_TYPE_OPTIONS = {
+    "Plant": DEVICE_TYPE_PLANT,
+    "Inverter": DEVICE_TYPE_INVERTER,
+}
+
+
 class FusionSolarPlusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     device_options = {}
 
